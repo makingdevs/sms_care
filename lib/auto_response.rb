1f70ca087e95beb75f9ce3475ba35a55e379a05a
neodevelop <neodevelop@gmail.com>
2016-07-01 11:04:13 -0500

    changing the message

--- lib/auto_response.rb
+++ lib/auto_response.rb
@@ -21,11 +21,7 @@ class AutoResponse
     if message # There's a message in our base of codes?
       message[1] # Return the message
     else
-      """Hola mecateros, would you like to help?
-      Funding: reply 111 [your msg]
-      Viral Messaging: reply 222 [your msg]
-      Community Engagement: reply 333 [your msg]
-      Recommend a Good Bar in MxCity: reply 444 [your msg]""" # Put a default message
+      """How can you help?  Funding: reply 111 [your msg] Viral: reply 222 [your msg] Community: reply 333 [your msg] Recommend Bar in MxCity: reply 444 [your msg]""" # Put a default message
     end
   end
 
