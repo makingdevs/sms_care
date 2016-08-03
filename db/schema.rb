cb470deff9b148a6ec92c7cfcace201a6f6f8f0d
neodevelop <neodevelop@gmail.com>
2016-08-03 17:17:36 -0500

    #6 adding the migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627223209) do
+ActiveRecord::Schema.define(version: 20160803171849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +27,13 @@ ActiveRecord::Schema.define(version: 20160627223209) do
 
   create_table "models", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "scheduled_messages", force: :cascade do |t|
+    t.string   "body",           limit: 140
+    t.string   "status",         limit: 50
+    t.string   "phone_number",   limit: 15
+    t.datetime "scheduled_date"
   end
 
 end
