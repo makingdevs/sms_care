4e421dc7c19bc280fe72ba9658f1a11169636ba4
neodevelop <neodevelop@gmail.com>
2016-08-03 17:17:55 -0500

    #6 Adding the migration file

---
+++ db/migrate/20160803171849_create_scheduled_messages.rb
@@ -0,0 +1,10 @@
+class CreateScheduledMessages < ActiveRecord::Migration
+  def change
+    create_table :scheduled_messages do |t| 
+      t.column :body, :string, limit: 140
+      t.column :status, :string, limit: 50
+      t.column :phone_number, :string, limit: 15
+      t.column :scheduled_date, :datetime
+    end
+  end
+end
